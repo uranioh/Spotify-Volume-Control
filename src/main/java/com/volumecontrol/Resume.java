1e2913f6fe2b1bf72df6423a1a38931d65a1e1ae
uranioh <melis.alessandro05@gmail.com>
2023-07-15 15:43:42 +0200

    Initial commit

---
+++ src/main/java/com/volumecontrol/Resume.java
@@ -0,0 +1,50 @@
+package com.volumecontrol;
+
+import authorization.client_credentials.ClientCredentialsLogin;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.requests.data.player.StartResumeUsersPlaybackRequest;
+import org.apache.hc.core5.http.ParseException;
+
+import java.io.IOException;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
+public class Resume {
+    private static final SpotifyApi spotifyApi = ClientCredentialsLogin.getSpotifyApi();
+    private static final StartResumeUsersPlaybackRequest startResumeUsersPlaybackRequest = spotifyApi
+            .startResumeUsersPlayback()
+            .build();
+
+    public static void startResumeUsersPlayback_Sync() {
+        try {
+            final String string = startResumeUsersPlaybackRequest.execute();
+
+            System.out.println("Null: " + string);
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void startResumeUsersPlayback_Async() {
+        try {
+            final CompletableFuture<String> stringFuture = startResumeUsersPlaybackRequest.executeAsync();
+
+            // Thread free to do other tasks...
+
+            // Example Only. Never block in production code.
+            final String string = stringFuture.join();
+
+            System.out.println("Null: " + string);
+        } catch (CompletionException e) {
+            System.out.println("Error: " + e.getCause().getMessage());
+        } catch (CancellationException e) {
+            System.out.println("Async operation cancelled.");
+        }
+    }
+
+    public static void main(String[] args) {
+        startResumeUsersPlayback_Sync();
+    }
+}
